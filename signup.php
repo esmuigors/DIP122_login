93899183ebbb7499207d0dc149c3fed685a4f1a6
esmuigors <igors.mihailovs0@gmail.com>
2018-04-19 14:51:06 +0300

    Adaptēju CSS izmantošanai

--- signup.php
+++ signup.php
@@ -1,12 +1,13 @@
 <html>
-<head><title>Reģistrācija sistēmā</title></head>
+<head><title>Reģistrācija sistēmā</title><link rel="stylesheet" type="text/css" href="borrowed_from_w3sch.css"></head>
 <body>
-<h1>Lūdzu ievadīt autorizācijas datus:</h1>
+<div class="container"><h1>Lūdzu ievadīt autorizācijas datus:</h1>
 
-<form method="post"> <!--pēc noklusējuma ir GET!-->
-<table><tr><td>Lietotāja vārds: </td><td><input type="text" name="username" placeholder="Me"> </td></tr>
-<tr><td>Parole: </td><td><input type="password" name="password" placeholder="qwerty"> </td></tr><tr><td>Parole vēlreiz: </td><td><input type="password" name="password2" placeholder="qwerty"> </td></tr></table>
-<input type="submit" name="sendlogin" value="Reģistrēties">
+<form method="post" style="border:1px solid #ccc"> <!--pēc noklusējuma ir GET!-->
+<table><tr><td><label for="username">Lietotāja vārds:</label> </td><td><input type="text" name="username" required="required" placeholder="Me"> </td></tr>
+<tr><td><label for="password">Parole:</label> </td><td><input type="password" name="password" required="required" placeholder="qwerty"> </td></tr>
+<tr><td><label for="password2">Parole vēlreiz:</label> </td><td><input type="password" name="password2" required="required" placeholder="qwerty"> </td></tr></table>
+<div class="clearfix"><input type="submit" name="sendlogin" value="Reģistrēties"></div>
 </form>
 
 <?php
@@ -38,5 +39,6 @@ else {
 
 ?>
 <p style="color:#999">Šī lapa vēl neizmanto sīkdatnes. Lūdzu, turpiniet un nedomājiet par tām!</p>
+</div>
 </body>
 </html>
