8b7c912a437687b3b6b8b26d038903aa1f24181b
esmuigors <igors.mihailovs0@gmail.com>
2018-04-19 13:22:36 +0300

    Reģistrācijas daļa

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+# session_start();
+$jaunsier=''; $vaiier=false; $vai2ier=false;
+$ieraksts=file_get_contents('user.txt'); #,$flags='FILE_IGNORE_NEW_LINES'
+( substr_count($ieraksts,"\r") == substr_count($ieraksts,"\n") ) ? $thisisunix="\r" : $thisisunix='';
+$haashy=substr(md5(rand()), 0, 16); # © tasmaniski@SO.com
+$papier=$user.' '.$password.' 1 '.$haashy.' false'.$thisisunix."\n";
+$ieraksts.=$papier;
+file_put_contents('user.txt',$ieraksts);
+echo '<h2>Jauns lietotājs ir veiksmīgi apstiprināts! Lūdzu pārbaudīt savu e-pasta kontu (patiesībā tūlīt atvērsies lapa ar saiti).</h2>';
+header('refresh:5;url=itkaepasts.php?skaties='.$haashy);
+?>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<html>
+<head><title>Reģistrācija sistēmā</title></head>
+<body>
+<h1>Lūdzu ievadīt autorizācijas datus:</h1>
+
+<form method="post"> <!--pēc noklusējuma ir GET!-->
+<table><tr><td>Lietotāja vārds: </td><td><input type="text" name="username" placeholder="Me"> </td></tr>
+<tr><td>Parole: </td><td><input type="password" name="password" placeholder="qwerty"> </td></tr><tr><td>Parole vēlreiz: </td><td><input type="password" name="password2" placeholder="qwerty"> </td></tr></table>
+<input type="submit" name="sendlogin" value="Reģistrēties">
+</form>
+
+<?php
+session_start();
+include 'ifnotuserblank.php';
+if ($userlogin) {
+	header('Location: logout.php');
+}
+else {
+	if (isset($_POST['sendlogin'])) {
+		$user=$_POST['username'];
+		echo '<br>'.$ier1.'<br>';
+		$password=$_POST['password'];
+		$password2=$_POST['password2'];
+		include 'onlyuser.php';
+		if ($userexists) {
+			echo 'Šāds lietotājs jau ir reģistrējies!';
+		}
+		else {
+			if ($password === $password2) {
+				include 'register.php';
+			}
+			else {
+				echo 'Ievadītās paroles nesakrīt! Lūdzu, pārbaudiet un ievadiet vēlreiz!';
+			}
+		}
+	}
+}
+
+?>
+<p style="color:#999">Šī lapa vēl neizmanto sīkdatnes. Lūdzu, turpiniet un nedomājiet par tām!</p>
+</body>
+</html>
