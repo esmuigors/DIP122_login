e363e02823976b48ecce9120f3d58ed0f3f75442
esmuigors <igors.mihailovs0@gmail.com>
2018-04-24 18:13:55 +0300

    Beidzot strādā!

--- counter.php
+++ counter.php
@@ -4,7 +4,7 @@ $ieraksts=file('user.txt'); #,$flags='FILE_IGNORE_NEW_LINES'
 ( substr_count(implode('',$ieraksts),"\r") == substr_count(implode('',$ieraksts),"\n") ) ? $thisisunix="\r" : $thisisunix='';
 foreach ($ieraksts as $noskey => $nosval) {
 		$nosavl=explode(' ',$nosval); #echo $nosavl[0].' '.$nosavl[3].' '.strlen(rtrim($nosavl[4])).'<br>'; 
-		if ($nosavl[1]==$_SESSION['user']) {
+		if ($nosavl[0]==$_SESSION['user']) {
 			$nosavl[6]=$nosavl[6]+1;
 			$plusss=$nosavl[0].' '.$nosavl[1].' '.$nosavl[2].' '.$nosavl[3].' '.$nosavl[4].' '.$nosavl[5].' '.$nosavl[6].$thisisunix."\n";
 		}
