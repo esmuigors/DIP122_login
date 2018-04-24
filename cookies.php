15f1776407034b62cc1b5703d3040b9fa61daf80
ashadorotea <38466036+ashadorotea@users.noreply.github.com>
2018-04-24 18:18:36 +0300

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -1,5 +1,10 @@
 <?php
-$cookie_name = $user;
-$cookie_value = $pass;
+$user = 'janis';
+$pass = 123;
+$cookie_name = 'user';
+$cookie_value = $user;
 setcookie($cookie_name, $cookie_value); 
+$cookie_name1 = 'password';
+$cookie_value1 = $pass;
+setcookie($cookie_name1, $cookie_value1); 
 ?>
