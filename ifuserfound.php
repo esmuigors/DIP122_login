b4ba2418b188a3f74ce3a41cb937c3c854c6ecf8
esmuigors <igors.mihailovs0@gmail.com>
2018-05-08 16:49:34 +0300

    password -> pass

--- ifuserfound.php
+++ ifuserfound.php
@@ -1,8 +1,8 @@
 <?php
 $f1="user.txt";
 $ieraksts = file_get_contents($f1);
-$ier1=$user." ".$password." 1";
-$ier2=$user." ".$password." 2";
+$ier1=$user." ".$pass." 1";
+$ier2=$user." ".$pass." 2";
 $_SESSION['dati']='';
 if (strpos($ieraksts,$ier1)!==false){
 	$_SESSION['dati']='1';
