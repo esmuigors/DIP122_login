6cb6fd203709082106e03b0748c642c49dbf5351
esmuigors <igors.mihailovs0@gmail.com>
2018-05-15 17:18:41 +0300

    it kā pielāgots db

--- valid_db.php
+++ valid_db.php
@@ -7,16 +7,18 @@
 $jaunsier=''; $vaiier=false; $vai2ier=false;
 include 'db.php';
 $sqlq = mysql_query("select hasshy, ifvaled from authen;");
-$valarr = array();
+$valarr = array(); $vaiier = 'false'; $vai2ier = 'false';
 
 while ($row_user = mysql_fetch_assoc($sqlq)){
     if ($row_user['hasshy']===$marko) {
 		if ($row_user['ifvaled']===0) {
-			$sqlq2 = mysql_query("insert into authen set ifvaled=1 where hasshy=".$marko.";");
+			$sqlq2 = mysql_query("insert into authen set ifvaled=1 where hasshy='".$marko."';");
 			if (!$sqlq2) {
 				echo "Could not validate user due to DB issues " . mysql_error();
 				exit;
 			}
+			echo '<h2>Jauns lietotājs ir veiksmīgi apstiprināts! Tūlīt tiks atvērta pierakstīšanās lapa.</h2>';
+			$vaiier=true;
 		}
 		else {
 			echo '<h2>Šāds lietotājs jau ir apstiprināts!</h2>';
@@ -26,31 +28,10 @@ while ($row_user = mysql_fetch_assoc($sqlq)){
 		break;
 	}
 }
-$ieraksts=file('user.txt'); #,$flags='FILE_IGNORE_NEW_LINES'
-( substr_count(implode('',$ieraksts),"\r") == substr_count(implode('',$ieraksts),"\n") ) ? $thisisunix="\r" : $thisisunix='';
-foreach ($ieraksts as $noskey => $nosval) {
-		$nosavl=explode(' ',$nosval); #echo $nosavl[0].' '.$nosavl[3].' '.strlen(rtrim($nosavl[4])).'<br>'; 
-		if ($nosavl[3]==$marko AND rtrim($nosavl[4])=='false') {
-			$plusss=$nosavl[0].' '.$nosavl[1].' '.$nosavl[2].' '.$nosavl[3].' true '.$nosavl[5].' '.$nosavl[6].$thisisunix."\n";
-			$vaiier=true;
-			#echo 'ir<br>';
-		} elseif ($nosavl[3]==$marko AND rtrim($nosavl[4])=='true') {
-			
-			#echo 'ir jau<br>';
-		} else {
-			$plusss=$nosval;
-			#echo 'nav<br>';
-		}
-		$jaunsier.=$plusss;
-		if ($vai2ier) break;
-	}
-	if ($vaiier and ! $vai2ier) {
-		file_put_contents('user.txt',$jaunsier);
-		echo '<h2>Jauns lietotājs ir veiksmīgi apstiprināts! Tūlīt tiks atvērta pierakstīšanās lapa.</h2>';
-	} elseif (! $vai2ier) {
-		echo '<h2>Šāds apstiprinājuma numurs nav atrasts datubāzē!</h2>';
-	}
-	header('refresh:5;url=login.php');
+if (not ($vaiier or $vai2ier)) {
+	echo '<h2>Šāds apstiprinājuma numurs nav atrasts datubāzē!</h2>';
+}
+header('refresh:5;url=login.php');
 ?>
 </body>
 </html>
