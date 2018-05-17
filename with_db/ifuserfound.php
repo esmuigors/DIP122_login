0e3fef048df18d549ed15175d6e635b66772c702
esmuigors <igors.mihailovs0@gmail.com>
2018-05-17 12:02:29 +0300

    drēbe karalim

--- with_db/ifuserfound.php
+++ with_db/ifuserfound.php
@@ -1,9 +1,13 @@
 <?php
 $_SESSION['dati']='';
-$sqlq = mysql_query("select usname, ifadmin from authen where usname='".$user."' and passy='".$pass."';");
+$sqlq = mysql_query("select usname, ifadmin, ifvaled from authen where usname='".$user."' and passy='".$pass."';");
 if ($sqlq) {
   $kasesmu=mysql_fetch_assoc($sqlq);
-	$_SESSION['dati']=$kasesmu['ifadmin'];
-  $_SESSION['user']=$user;
+  if ($kasesmu['ifvaled']==='1') {
+  	$_SESSION['dati']=$kasesmu['ifadmin'];
+  	$_SESSION['user']=$user;
+  } else {
+	echo "<h2>Nav saņemts apstiprinājums no lietotāja e-pasta!</h2>";
+  }
 }
 ?>
