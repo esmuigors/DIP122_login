4053978c5fbe75fbc3683693d4460188297fa818
esmuigors <igors.mihailovs0@gmail.com>
2018-05-17 13:00:29 +0300

    Create register.php

---
+++ with_db/register.php
@@ -0,0 +1,12 @@
+
+<?php
+$haashy=substr(md5(rand()), 0, 16); # © tasmaniski@SO.com
+$sqlq2 = mysqli_query("insert into authen values ('".$user."','".$pass."',0,'".$haashy."',0,0,0)");
+if (!$sqlq2) {
+ 				echo "Nevar reģistrēt lietotāju SQL problēmu dēļ: " . mysql_error();
+ 				exit;
+}
+mysqli_free_result($sqlq2);
+echo '<h2>Jauns lietotājs ir veiksmīgi reģistrēts! Lūdzu pārbaudīt savu e-pasta kontu (patiesībā tūlīt atvērsies lapa ar saiti).</h2>';
+header('refresh:5;url=itkaepasts.php?skaties='.$haashy);
+?>
