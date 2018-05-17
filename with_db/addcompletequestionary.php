eb77e9a2b9424d9302d106a747f6337a1ac030fa
esmuigors <igors.mihailovs0@gmail.com>
2018-05-17 13:35:45 +0300

    Update addcompletequestionary.php

--- with_db/addcompletequestionary.php
+++ with_db/addcompletequestionary.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_GET['nos'])) {
+if (isset($_POST['galaa'])) {
 	$sqlq=mysqli_query("select count(*) from responses");
   $idiks=mysqli_fetch_assoc($sqlq)+1;
   mysqli_free_result($sqlq);
-  $sqlq = mysqli_query("insert into responses values (".$idiks.",'".$user."','".$_GET['nos']."','".$_GET['r1']."','".$_GET['r2']."','".$_GET['r3']."')");
+  $sqlq = mysqli_query("insert into responses values (".$idiks.",'".$user."','".$_POST['nos']."','".$_POST['r1']."','".$_POST['r2']."','".$_POST['r3']."')");
   if (!$sqlq) {
- 				echo "Diemžēl nebija iespējams saglabāt aptaujas rezultātus SQL problēmu dēļ: " . mysql_error();
- 				exit;
+ 	echo "Diemžēl nebija iespējams saglabāt aptaujas rezultātus SQL problēmu dēļ: " . mysql_error();
+ 	exit;
   }
   mysqli_free_result($sqlq);
 }
